c58e9f498542b5dced599c41e2b54d8010417c3e
Chaz Logan <chazaqlogan@gmail.com>
2021-04-24 20:49:17 -0400

    Solving Cont...

--- doubledValues.js
+++ doubledValues.js
@@ -180,3 +180,14 @@
 
 
 // Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+
+// const find = function (arr, val) {
+
+//     return arr.filter(function (el) {
+//         return el === val
+//     })[0];
+
+// }
+
+// console.log(find([1, 2, 3, 4, 5], 3)) // 3
+// console.log(find([1, 2, 3, 4, 5], 10)) // undefined

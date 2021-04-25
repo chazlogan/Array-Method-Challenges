ab7469f6225c1b4869caff03ba70b5d481008325
Chaz Logan <chazaqlogan@gmail.com>
2021-04-24 20:27:47 -0400

    Solving Cont1

--- doubledValues.js
+++ doubledValues.js
@@ -145,4 +145,38 @@
 // };
 
 
-Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
+// Write a function called filterByValue which accepts an array of objects and a key and returns a new array with all the objects that contain that key.
+
+
+// const filterByValue = function (arr, key) {
+
+//     return arr.filter(function (value) {
+//         return value[key] !== undefined;
+
+
+//     });
+// }
+
+
+// console.log(filterByValue(
+//     [
+//         { first: 'Elie', last: "Schoppik" },
+//         { first: 'Tim', last: "Garcia", isCatOwner: true },
+//         { first: 'Matt', last: "Lane" },
+//         { first: 'Colt', last: "Steele", isCatOwner: true }
+//     ],
+// //     'isCatOwner'
+// // ))
+
+// const filterByValue = function (arr, key) {
+
+//     return arr.filter(function (val) {
+
+//         return val[key] !== undefined;
+
+
+//     });
+// }
+
+
+// Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
